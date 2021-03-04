dfc11758961d80cf9ab57a68139b73105c1b5c43
digiarganda <80050359+digiarganda@users.noreply.github.com>
2021-03-04 17:03:20 +0100

    Add files via upload

---
+++ dialogflow-heroku-fulfillment.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {WebhookClient} = require('dialogflow-fulfillment');
+
+const app = express()
+app.use(bodyParser.json())
+const port = process.env.PORT || 3000
+
+app.post('/dialogflow-fulfillment', (request, response) => {
+    dialogflowFulfillment(request, response)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+const dialogflowFulfillment = (request, response) => {
+    const agent = new WebhookClient({request, response})
+
+    function sayHello(agent){
+        agent.add("Hello, this was a nice tutorial by axlewebtech")
+    }
+
+    let intentMap = new Map();
+    intentMap.set("Default Welcome Intent", sayHello)
+    agent.handleRequest(intentMap)
+
+}
