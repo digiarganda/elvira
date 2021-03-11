a8969b35e68472d2f768cb27663f07c3de5b8703
digiarganda <80050359+digiarganda@users.noreply.github.com>
2021-03-11 13:56:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ restService.post("/echo", function(req, res) {
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
       ? req.body.queryResult.parameters.echoText
-      : "Ha habido algunos problemas. Vuelve a intentarlo"+req.body;
+  :https://datosabiertos.ayto-arganda.es/api/3/action/group_list;
+     // : "Ha habido algunos problemas. Vuelve a intentarlo"+req.body;
   return res.json({
 
   "fulfillmentText": speech,
